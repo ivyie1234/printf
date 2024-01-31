e74ff797acd8d68b44e868225a0b5f0d20e19b3e
Obarechrispus <obarechrispus@gmail.com>
2024-01-30 16:11:07 -0800

    t

--- c_print.c
+++ c_print.c
@@ -8,73 +8,53 @@
 int _printf(const char *format, ...)
 {
 	int chara = 0;
-	va_list args;
+		va_list args;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
-
-	va_start(args, format);
-
-/*	while (*format)
-    {
-        if (*format != '%')
-        {
-            chara += put_char(*format);
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == 'c')
-            {
-                chara += _printf_char(va_arg(args, int));
-            }
-            else if (*format == 's')
-            {
-                chara += _printf_str(va_arg(args, char *));
-            }*/
-
-	while (*format)
-	{
-		if (*format != '%')
+		if (format == NULL)
 		{
-			put_char(*format);
-			chara++;
+				return (-1);
 		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-			{
-				put_char(*format);
-				chara++;
-			}
-			else if (*format == 'c')
-			{
-				char c = _printf_char(va_arg(args, int));
-				put_char(c);
-				chara++;
-			}
-			else if (*format == 's')
-			{
-				char *str = (va_arg(args, char*));
-				/*int len = 0;
 
-				while (str[len] != '\0')
-					len++;
-				write(1, str, len);
-				chara += len;*/
-				while (*str)
+		va_start(args, format);
+
+		while (*format)
+		{
+					if (*format != '%')
 				{
-				put_char(*str);
-				chara += _printf_str(str);
-				str++;
+						put_char(*format);
+						chara++;
 				}
+				else
+				{
+						format++;
+						if (*format == '\0')
+								break;
+						if (*format == '%')
+						{
+								put_char(*format);
+								chara++;
+						}
+						else if (*format == 'c')
+						{
+								char c = va_arg(args, int);
+								put_char(c);
+								chara++;
+						}
+						else if (*format == 's')
+						{
+								char *str = va_arg(args, char*);
+								/*int len = 0;
+
+								while (str[len] != '\0')
+										len++;
+								write(1, str, len);
+								chara += len;*/
+								while (*str)
+								{
+								put_char(*str);
+								chara++;
+								str++;
+								}
 			}
 			else if (*format == 'd' || *format == 'i')
 			{
@@ -84,26 +64,26 @@ int _printf(const char *format, ...)
 				write(1, num_str, num_len);
 				chara += num_len;
 			}
-			            else if (*format == 'b')
-            {
-                chara += _printf_bin(args);
-            }
-				                else if (*format == 'u')
-            {
-                chara += _printf_u(args);
-            }
-            else if (*format == 'o')
-            {
-                chara += _printf_o(args);
-            }
-            else if (*format == 'x')
-            {
-                chara += _printf_x(args);
-            }
-            else if (*format == 'X')
-            {
-                chara += _printf_X(args);
-            }
+						else if (*format == 'b')
+			{
+				chara += _printf_bin(args);
+			}
+								else if (*format == 'u')
+			{
+				chara += _printf_u(args);
+			}
+			else if (*format == 'o')
+			{
+				chara += _printf_o(args);
+			}
+			else if (*format == 'x')
+			{
+				chara += _printf_x(args);
+			}
+			else if (*format == 'X')
+			{
+				chara += _printf_X(args);
+			}
 		}
 		format++;
 	}
